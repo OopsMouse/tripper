177cdf80b432b5f0228d168405ea75d66ae7db0c
Noda Shimpei <noda.sin@gmail.com>
2014-04-28 23:00:24 +0900

    GruntにOpenを追加．

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,52 @@
 'use strict';
 
 module.exports = function(grunt) {
-	grunt.initConfig({
-		jshint: {
-			files: [
+  grunt.initConfig({
+    jshint: {
+      files: [
         'Gruntfile.js',
         'routes/**/*.js',
         'package.json',
         'test/**/*.js',
         '*.js',
         '.jshintrc'
-			],
+      ],
       options: {
         jshintrc: '.jshintrc'
       }
-		},
-		exec: {
-			express: {
-				cmd: 'npm start',
-				stdout:  true,
-				stderror: true
-			}
-		},
-		watch: {
-			files: [ '**/*.jade', '**/*.js' ],
-      tasks: [ 'exec:express' ]
+    },
+    exec: {
+      express: {
+        cmd: 'node dev.js',
+        stdout:  true,
+        stderror: true
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      jade: {
+        files: [
+          'views/*.jade'
+        ]
+      },
+      scripts: {
+        files: [
+          '*.js',
+          'routes/*.js'
+        ]
+      },
+    },
+    open: {
+      server: {
+        path: 'http://localhost:3000'
+      }
     }
-	});
-	grunt.loadNpmTasks('grunt-exec');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.registerTask('default', [ 'jshint', 'watch' ]);
+  });
+  grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('development', [ 'watch' ]);
 };
